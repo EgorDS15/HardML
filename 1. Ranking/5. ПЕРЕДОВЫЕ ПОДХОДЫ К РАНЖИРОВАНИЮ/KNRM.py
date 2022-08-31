e86d274b9f5eb1af4f6691a0046f9912a45016c8
Egor <ovchinnikovegords@gmail.com>
2022-08-31 17:03:44 +0300

    added author solution

--- 1. Ranking/5. ПЕРЕДОВЫЕ ПОДХОДЫ К РАНЖИРОВАНИЮ/KNRM.py
+++ 1. Ranking/5. ПЕРЕДОВЫЕ ПОДХОДЫ К РАНЖИРОВАНИЮ/KNRM.py
@@ -9,53 +9,90 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 
-# Замените пути до директорий и файлов! Можете использовать для локальной отладки.
-# При проверке на сервере пути будут изменены
-glue_qqp_dir = '/data/QQP/'
-glove_path = '/data/glove.6B.50d.txt'
+# from tqdm.auto import tqdm
+
+
+glue_qqp_dir = '/Users/igor.kotenkov/Documents/_WORKSPACES/courses/homework/additional_data/QQP/'
+glove_path = '/Users/igor.kotenkov/Documents/_WORKSPACES/courses/homework/additional_data/glove.6B.50d.txt'
+
+
+# glue_qqp_dir = '/additional_data/QQP/'
+# glove_path = '/additional_data/glove.6B.50d.txt'
 
 
 class GaussianKernel(torch.nn.Module):
+    """
+    Основа архитектуры - одно гаусово ядро, которое будет сварачивать нашу матрицу матчинга и будет получать
+    активации, поверх которых будем строить лиенейный классификатор
+    """
     def __init__(self, mu: float = 1., sigma: float = 1.):
         super().__init__()
         self.mu = mu
         self.sigma = sigma
 
     def forward(self, x):
-        # допишите ваш код здесь 
-        pass
+        # смотрим как текущий признак отличается от центра ядра: x - self.mu. Чем дальше значение от ядра, тем меньше
+        # значение на выходе. Ширина "купола": self.sigma ** 2
+        return torch.exp(-0.5 * ((x - self.mu) ** 2) / (self.sigma ** 2))
 
 
 class KNRM(torch.nn.Module):
-    def __init__(self, embedding_matrix: np.ndarray, freeze_embeddings: bool, kernel_num: int = 21,
-                 sigma: float = 0.1, exact_sigma: float = 0.001,
+    def __init__(self,
+                 embedding_matrix: np.ndarray,
+                 freeze_embeddings: bool,
+                 kernel_num: int = 21,
+                 sigma: float = 0.1,
+                 exact_sigma: float = 0.001,
                  out_layers: List[int] = [10, 5]):
+        """
+        :param embedding_matrix: матрица эмбеддингов N*D. N - размер словаря эмбединга, D - размер вектора в этой матрице
+        :param freeze_embeddings: хотим ли дообучать эмбединги
+        :param kernel_num: количество ядер
+        :param sigma: параметр ядра
+        :param exact_sigma:
+        :param out_layers: указывает на структуру финального линейного слоя
+        """
         super().__init__()
+        # перевод предобученных numpy эмбедингов в torch эмбединги
         self.embeddings = torch.nn.Embedding.from_pretrained(
             torch.FloatTensor(embedding_matrix),
             freeze=freeze_embeddings,
-            padding_idx=0
-        )
+            padding_idx=0)
 
         self.kernel_num = kernel_num
         self.sigma = sigma
         self.exact_sigma = exact_sigma
         self.out_layers = out_layers
-
         self.kernels = self._get_kernels_layers()
-
         self.mlp = self._get_mlp()
-
         self.out_activation = torch.nn.Sigmoid()
 
     def _get_kernels_layers(self) -> torch.nn.ModuleList:
+        # Создание ядер. Используя Гаусово ядро создаем список ядер - torch.nn.ModuleList().
+        # Но нужно понять по какой формуле расчитывается значение mu - центр ядра, на которое
+        # обращается внимание. Как находим формулу - числа от -1 до 1. рсаполагаем числа в интервал от 0 до 1,
+        # удваиваем и отнимаем 1, тем самым сдвигаем влево шкалу.
         kernels = torch.nn.ModuleList()
-        # допишите ваш код здесь 
+        for i in range(self.kernel_num):
+            mu = 1. / (self.kernel_num - 1) + (2. * i) / (self.kernel_num - 1) - 1.0
+            sigma = self.sigma
+            if mu > 1.0:
+                sigma = self.exact_sigma
+                mu = 1.0
+            kernels.append(GaussianKernel(mu=mu, sigma=sigma))
         return kernels
 
     def _get_mlp(self) -> torch.nn.Sequential:
-        # допишите ваш код здесь
-        pass
+        out_cont = [self.kernel_num] + self.out_layers + [1]
+        mlp = [
+            torch.nn.Sequential(
+                torch.nn.Linear(in_f, out_f),
+                torch.nn.ReLU()
+            )
+            for in_f, out_f in zip(out_cont, out_cont[1:])
+        ]
+        mlp[-1] = mlp[-1][:-1]
+        return torch.nn.Sequential(*mlp)
 
     def forward(self, input_1: Dict[str, torch.Tensor], input_2: Dict[str, torch.Tensor]) -> torch.FloatTensor:
         logits_1 = self.predict(input_1)
@@ -67,8 +104,18 @@ class KNRM(torch.nn.Module):
         return out
 
     def _get_matching_matrix(self, query: torch.Tensor, doc: torch.Tensor) -> torch.FloatTensor:
-        # допишите ваш код здесь 
-        pass
+        # shape = [B, L, D]
+        embed_query = self.embeddings(query.long())
+        # shape = [B, R, D]
+        embed_doc = self.embeddings(doc.long())
+
+        # shape = [B, L, R]
+        matching_matrix = torch.einsum(
+            'bld,brd->blr',
+            F.normalize(embed_query, p=2, dim=-1),
+            F.normalize(embed_doc, p=2, dim=-1)
+        )
+        return matching_matrix
 
     def _apply_kernels(self, matching_matrix: torch.FloatTensor) -> torch.FloatTensor:
         KM = []
@@ -82,14 +129,12 @@ class KNRM(torch.nn.Module):
         return kernels_out
 
     def predict(self, inputs: Dict[str, torch.Tensor]) -> torch.FloatTensor:
-        # shape = [Batch, Left, Embedding], [Batch, Right, Embedding]
         query, doc = inputs['query'], inputs['document']
-
-        # shape = [Batch, Left, Right]
+        # shape = [B, L, R]
         matching_matrix = self._get_matching_matrix(query, doc)
-        # shape = [Batch, Kernels]
+        # shape [B, K]
         kernels_out = self._apply_kernels(matching_matrix)
-        # shape = [Batch]
+        # shape [B]
         out = self.mlp(kernels_out)
         return out
 
@@ -109,12 +154,13 @@ class RankingDataset(torch.utils.data.Dataset):
         return len(self.index_pairs_or_triplets)
 
     def _tokenized_text_to_index(self, tokenized_text: List[str]) -> List[int]:
-        # допишите ваш код здесь
-        pass
+        res = [self.vocab.get(i, self.oov_val) for i in tokenized_text]
+        return res
 
     def _convert_text_idx_to_token_idxs(self, idx: int) -> List[int]:
-        # допишите ваш код здесь
-        pass
+        tokenized_text = self.preproc_func(self.idx_to_text_mapping[idx])
+        idxs = self._tokenized_text_to_index(tokenized_text)
+        return idxs
 
     def __getitem__(self, idx: int):
         pass
@@ -122,14 +168,31 @@ class RankingDataset(torch.utils.data.Dataset):
 
 class TrainTripletsDataset(RankingDataset):
     def __getitem__(self, idx):
-        # допишите ваш код здесь
-        pass
+        cur_row = self.index_pairs_or_triplets[idx]
+        left_idxs = self._convert_text_idx_to_token_idxs(cur_row[0])[
+                    :self.max_len]
+        r1_idxs = self._convert_text_idx_to_token_idxs(cur_row[1])[
+                  :self.max_len]
+        r2_idxs = self._convert_text_idx_to_token_idxs(cur_row[2])[
+                  :self.max_len]
+
+        pair_1 = {'query': left_idxs, 'document': r1_idxs}
+        pair_2 = {'query': left_idxs, 'document': r2_idxs}
+        target = cur_row[3]
+        return (pair_1, pair_2, target)
 
 
 class ValPairsDataset(RankingDataset):
     def __getitem__(self, idx):
-        # допишите ваш код здесь
-        pass
+        cur_row = self.index_pairs_or_triplets[idx]
+        left_idxs = self._convert_text_idx_to_token_idxs(cur_row[0])[
+                    :self.max_len]
+        r1_idxs = self._convert_text_idx_to_token_idxs(cur_row[1])[
+                  :self.max_len]
+
+        pair = {'query': left_idxs, 'document': r1_idxs}
+        target = cur_row[2]
+        return (pair, target)
 
 
 def collate_fn(batch_objs: List[Union[Dict[str, torch.Tensor], torch.FloatTensor]]):
@@ -203,28 +266,6 @@ class Solution:
                  train_lr: float = 0.001,
                  change_train_loader_ep: int = 10
                  ):
-        """
-            min_token_occurancies — минимальное количество раз, которое слово (токен) должно появиться в выборке,
-            чтобы не быть отброшенным как низкочастотное. При значении, равном единице, остаются все слова, которые
-            представлены в датасете.
-
-            emb_rand_uni_bound — половина ширины интервала, из которого равномерно (uniform) генерируются вектора
-            эмбеддингов (если вектор не представлен в наборе GloVe). Если параметр равен 0.2, то каждая компонента
-            вектора принадлежит U(−0.2,0.2)
-
-            freeze_knrm_embeddings — флаг, указывающий на необходимость дообучения эмбеддингов, будут ли по ним считаться
-            градиенты (при True дообучение происходить не будет).
-
-            knrm_kernel_num — количество ядер в KNRM.
-
-            knrm_out_mlp — конфигурация MLP-слоя на выходе в KNRM. Детали см. ниже.
-
-            dataloader_bs — размер батча при обучении и валидации модели.
-
-            train_lr — Learning Rate, использующийся при обучении модели KNRM.
-
-            change_train_loader_ep — как часто менять/перегенерировать выборку для тренировки модели. Детали см. ниже.
-        """
         self.glue_qqp_dir = glue_qqp_dir
         self.glove_vectors_path = glove_vectors_path
         self.glue_train_df = self.get_glue_df('train')
@@ -257,7 +298,7 @@ class Solution:
             self.val_dataset, batch_size=self.dataloader_bs, num_workers=0,
             collate_fn=collate_fn, shuffle=False)
 
-    def get_glue_df(self, partition_type: str) -> pd.DataFrame:
+    def get_glue_df(self, partition_type: str):
         assert partition_type in ['dev', 'train']
         glue_df = pd.read_csv(
             self.glue_qqp_dir + f'/{partition_type}.tsv', sep='\t', error_bad_lines=False, dtype=object)
@@ -272,69 +313,68 @@ class Solution:
         return glue_df_fin
 
     def hadle_punctuation(self, inp_str: str) -> str:
-        for i in string.punctuation:
-            inp_str = inp_str.replace(i, '').strip()
+        inp_str = str(inp_str)
+        for punct in string.punctuation:
+            inp_str = inp_str.replace(punct, ' ')
         return inp_str
 
-    def simple_preproc(self, inp_str: str) -> List[str]:
-        inp_str = self.hadle_punctuation(inp_str).lower()
-        return nltk.word_tokenize(inp_str)
+    def simple_preproc(self, inp_str: str):
+        base_str = inp_str.strip().lower()
+        str_wo_punct = self.hadle_punctuation(base_str)
+        return nltk.word_tokenize(str_wo_punct)
 
-    def _filter_rare_words(self, vocab: Dict[str, int], min_occurancies: int) -> Dict[str, int]:
-        if min_occurancies < 1:
-            raise ValueError("Parameter min_occurancies should be int type and has a value 1 or bigger")
-
-        return {k: v for k, v in vocab.items() if v >= min_occurancies}
+    def _filter_rare_words(self, vocab: dict, min_occurancies: int) -> dict:
+        out_vocab = dict()
+        for word, cnt in vocab.items():
+            if cnt >= min_occurancies:
+                out_vocab[word] = cnt
+        return out_vocab
 
     def get_all_tokens(self, list_of_df: List[pd.DataFrame], min_occurancies: int) -> List[str]:
-        # уникальное множество
-        uniq_text = set()
+        def flatten(t): return [item for sublist in t for item in sublist]
+
+        tokens = []
         for df in list_of_df:
-            for colmn in df:
-                for row in df[colmn]:
-                    uniq_text.add(str(row))
-
-        # разбиение на токены, обрезание пробела и приведение к нижнему регистру
-        prep_text = []
-        for i in uniq_text:
-            prep_text.append([i for i in self.simple_preproc(i)])
-
-        # extracted tokens
-        all_tokens = [item for sublist in prep_text for item in sublist]
-
-        # считаем количество
-        freq_table = {}
-        for word in all_tokens:
-            if word in freq_table:
-                freq_table[word] += 1
-            else:
-                freq_table[word] = 1
-        # Фильтруем по частоте
-        result = [key for key in self._filter_rare_words(freq_table, min_occurancies).keys()]
-
-        return result
+            unique_texts = set(
+                df[['text_left', 'text_right']].values.reshape(-1))
+            df_tokens = flatten(map(self.simple_preproc, unique_texts))
+            tokens.extend(list(df_tokens))
+        count_filtered = self._filter_rare_words(
+            Counter(tokens), min_occurancies)
+        return list(count_filtered.keys())
 
     def _read_glove_embeddings(self, file_path: str) -> Dict[str, List[str]]:
-        lines = []
+        embedding_data = {}
+
         with open(file_path, 'r', encoding='utf-8') as f:
             for line in f:
-                lines.append(line.rstrip())
-
-        splited_line = []
-        for emb in lines:
-            splited_line.append(emb.split())
-
-        dct = dict()
-        for i in splited_line:
-            dct[i[0]] = i[1:]
-
-        return dct
+                current_line = line.rstrip().split(' ')
+                embedding_data[current_line[0]] = current_line[1:]
+        return embedding_data
 
     def create_glove_emb_from_file(self, file_path: str, inner_keys: List[str],
                                    random_seed: int, rand_uni_bound: float
                                    ) -> Tuple[np.ndarray, Dict[str, int], List[str]]:
-        # допишите ваш код здесь
-        pass
+        glove_emb = self._read_glove_embeddings(file_path)
+
+        inner_keys = ['PAD', 'OOV'] + inner_keys
+        input_dim = len(inner_keys)
+        out_dim = len(list(glove_emb.values())[0])
+        vocab = dict()
+        np.random.seed(random_seed)
+        matrix = np.empty((input_dim, out_dim))
+        unk_words = []
+
+        for idx, word in enumerate(inner_keys):
+            vocab[word] = idx
+            if word in glove_emb:
+                matrix[idx] = glove_emb[word]
+            else:
+                unk_words.append(word)
+                matrix[idx] = np.random.uniform(-rand_uni_bound,
+                                                rand_uni_bound, size=out_dim)
+        matrix[0] = np.zeros_like(matrix[0])
+        return matrix, vocab, unk_words
 
     def build_knrm_model(self) -> Tuple[torch.nn.Module, Dict[str, int], List[str]]:
         emb_matrix, vocab, unk_words = self.create_glove_emb_from_file(
@@ -346,8 +386,35 @@ class Solution:
 
     def sample_data_for_train_iter(self, inp_df: pd.DataFrame, seed: int
                                    ) -> List[List[Union[str, float]]]:
-        # допишите ваш код здесь
-        pass
+        groups = inp_df[['id_left', 'id_right', 'label']].groupby('id_left')
+        pairs_w_labels = []
+        np.random.seed(seed)
+        all_right_ids = inp_df.id_right.values
+        for id_left, group in groups:
+            labels = group.label.unique()
+            if len(labels) > 1:
+                for label in labels:
+                    same_label_samples = group[group.label ==
+                                               label].id_right.values
+                    if label == 0 and len(same_label_samples) > 1:
+                        sample = np.random.choice(
+                            same_label_samples, 2, replace=False)
+                        pairs_w_labels.append(
+                            [id_left, sample[0], sample[1], 0.5])
+                    elif label == 1:
+                        less_label_samples = group[group.label <
+                                                   label].id_right.values
+                        pos_sample = np.random.choice(
+                            same_label_samples, 1, replace=False)
+                        if len(less_label_samples) > 0:
+                            neg_sample = np.random.choice(
+                                less_label_samples, 1, replace=False)
+                        else:
+                            neg_sample = np.random.choice(
+                                all_right_ids, 1, replace=False)
+                        pairs_w_labels.append(
+                            [id_left, pos_sample[0], neg_sample[0], 1])
+        return pairs_w_labels
 
     def create_val_pairs(self, inp_df: pd.DataFrame, fill_top_to: int = 15,
                          min_group_size: int = 2, seed: int = 0) -> List[List[Union[str, float]]]:
@@ -385,28 +452,26 @@ class Solution:
         return out_pairs
 
     def get_idx_to_text_mapping(self, inp_df: pd.DataFrame) -> Dict[str, str]:
-        left_dict = (
-            inp_df
-            [['id_left', 'text_left']]
-                .drop_duplicates()
-                .set_index('id_left')
-            ['text_left']
-                .to_dict()
-        )
-        right_dict = (
-            inp_df
-            [['id_right', 'text_right']]
-                .drop_duplicates()
-                .set_index('id_right')
-            ['text_right']
-                .to_dict()
-        )
+        left_dict = inp_df[['id_left', 'text_left']].drop_duplicates().set_index('id_left')[
+            'text_left'].to_dict()
+        right_dict = inp_df[['id_right', 'text_right']].drop_duplicates(
+        ).set_index('id_right')['text_right'].to_dict()
         left_dict.update(right_dict)
         return left_dict
 
     def ndcg_k(self, ys_true: np.array, ys_pred: np.array, ndcg_top_k: int = 10) -> float:
-        # допишите ваш код здесь  (обратите внимание, что используются вектора numpy)
-        pass
+        def dcg(ys_true, ys_pred):
+            argsort = np.argsort(ys_pred)[::-1]
+            argsort = argsort[:ndcg_top_k]
+            ys_true_sorted = ys_true[argsort]
+            ret = 0
+            for i, l in enumerate(ys_true_sorted, 1):
+                ret += (2 ** l - 1) / math.log2(1 + i)
+            return ret
+
+        ideal_dcg = dcg(ys_true, ys_true)
+        pred_dcg = dcg(ys_true, ys_pred)
+        return (pred_dcg / ideal_dcg)
 
     def valid(self, model: torch.nn.Module, val_dataloader: torch.utils.data.DataLoader) -> float:
         labels_and_groups = val_dataloader.dataset.index_pairs_or_triplets
@@ -434,5 +499,39 @@ class Solution:
     def train(self, n_epochs: int):
         opt = torch.optim.SGD(self.model.parameters(), lr=self.train_lr)
         criterion = torch.nn.BCELoss()
-        # допишите ваш код здесь 
-        pass
+        ndcgs = []
+        for ep in range(n_epochs):
+            if ep % self.change_train_loader_ep == 0:
+                sampled_train_triplets = self.sample_data_for_train_iter(self.glue_train_df, seed=ep)
+                train_dataset = TrainTripletsDataset(sampled_train_triplets,
+                                                     self.idx_to_text_mapping_train,
+                                                     vocab=self.vocab, oov_val=self.vocab['OOV'],
+                                                     preproc_func=self.simple_preproc)
+                train_dataloader = torch.utils.data.DataLoader(
+                    train_dataset, batch_size=self.dataloader_bs, num_workers=0,
+                    collate_fn=collate_fn, shuffle=True, )
+
+            for batch in (train_dataloader):
+                inp_1, inp_2, y = batch
+                preds = self.model(inp_1, inp_2)
+                loss = criterion(preds, y)
+                loss.backward()
+                opt.step()
+            ndcg = self.valid(self.model, self.val_dataloader)
+            ndcgs.append(ndcg)
+            if ndcg > 0.925:
+                break
+
+# sol = Solution(glue_qqp_dir, glove_path, knrm_out_mlp=[])
+
+# sol.train(10)
+# state_mlp = sol.model.mlp.state_dict()
+# torch.save(state_mlp, open('../lec11/user_input/knrm_mlp.bin', 'wb'))
+
+# state_emb = sol.model.embeddings.state_dict()
+# # torch.save(state_emb, open('../lec11/user_input/knrm_emb.bin', 'wb'))
+# torch.save(state_emb, open('../additional_data/lec11/knrm_emb.bin', 'wb'))
+
+# import json
+# state_vocab = sol.vocab
+# json.dump(state_vocab, open('../additional_data/lec11/vocab.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
